524a28e08dae06802212e42dea66b4758bbdcafe
Eunchae <ddungal3@gmail.com>
2023-11-29 13:42:09 +0900

    Feat: 페이지 라우팅, 컴포넌트 생성

---
+++ src/components/Router.jsx
@@ -0,0 +1,30 @@
+import LoginPage from "@pages/LoginPage";
+import SignupPage from "@pages/SignupPage";
+import MainPage from "@pages/MainPage";
+import PostPage from "@pages/PostPage";
+import CategoryPage from "@pages/CategoryPage";
+import DetailPage from "@pages/DetailPage";
+import PaymentPage from "@pages/PaymentPage";
+import ProfilePage from "@pages/ProfilePage";
+import AdminPage from "@pages/AdminPage";
+import ChatPage from "@pages/ChatPage";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/post" element={<PostPage />} />
+      <Route path="/category" element={<CategoryPage />} />
+      <Route path="/detail/:id" element={<DetailPage />} />
+      <Route path="/payment" element={<PaymentPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="*" element={<Navigate replace to="/login"/>}/>
+    </Routes>
+  );
+}
---
+++ src/components/ticket/TicketCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TicketCard() {
+  return (
+    <div>TicketCard</div>
+  )
+}
