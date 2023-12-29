46b199b51ae4a746393c5c4efc19545cc02527b0
Eunchae <ddungal3@gmail.com>
2023-12-29 19:05:56 +0900

    :alien: DetailPage에 판매글조회 API 연동 (현재 에러발생),  신고 버튼 옆에 "기타"항목 추가

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { useRecoilValue, useRecoilState } from "recoil";
-import fetchPostsData from "@utils/fetchPostsData.jsx";
-import { postsDataState } from "@recoil/postsDataState.jsx";
+import axios from 'axios';
+// import { useRecoilState } from "recoil";
+// import fetchPostsData from "@utils/fetchPostsData.jsx";
+// import { postsDataState } from "@recoil/postsDataState.jsx";
 import Navbar from "@components/common/Navbar.jsx";
 import ReportBtn from "@assets/img_btn_report.png";
 import { formatDate } from "@utils/formatDate.js";
@@ -12,76 +12,77 @@ export default function DetailPage() {
   const { salePostId } = useParams();
   const numericSalePostId = Number(salePostId);
 
-  const [postsData, setPostsData] =
-    useRecoilState(postsDataState);
+  const [loading, setLoading] = useState(false);
 
-  const [filteredData, setFilteredData] = useState({});
+  const [detailData, setDetailData] = useState(null);
 
-  useEffect(() => {
-    const loadData = async () => {
+    const fetchData = async () => {
+      console.log("fetchData 함수가 호출되었습니다.");
       try {
-        await fetchPostsData(setPostsData);
+        setLoading(true);
+        const response = await axios.get(
+          `https://oriticket.link/posts?&id=${salePostId}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        console.log("API 응답:", response);
+
+        if (!response.data || !response.data.ticket) {
+          throw new Error("Invalid data structure");
+        }
+
+        const newData = response.data;
+        setDetailData(newData);
+        console.log("detailData가 설정되었습니다:", detailData);
+
       } catch (error) {
-        console.error("Fetching posts data failed:", error);
+        console.error("API 호출 중 에러가 발생했습니다:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    loadData();
-  }, [setPostsData]);
-
   useEffect(() => {
-    if (postsData) {
-      const data = postsData.find(
-        (post) => post.salePostId === numericSalePostId
-      );
-
-      setFilteredData(data); // 찾은 데이터로 상태 업데이트
-    }
-  }, [postsData, numericSalePostId]);
+    console.log('useEffect가 실행되었습니다.');
+    fetchData();
+  }, []);
 
   return (
     <div>
       <Navbar />
       <div className="max-w-5xl mb-4">
-        <h1 className="text-navy-basic font-extrabold text-5xl">
-          ORI-TICKET
-        </h1>
-        <h1 className="text-navy-basic font-extrabold text-5xl">
-          상품정보
-        </h1>
+        <span className="text-navy-basic font-extrabold text-4xl">티켓 상세정보</span>
 
-        <div className="card-compact w-full my-4 bg-base-100 shadow-xl">
+        <div className="card-compact w-full my-6 bg-base-100 shadow-xl">
           <div className="card-body">
             <div className="flex">
               <div className="text-xl font-extrabold pt-1">
-                {filteredData.sportsName}&nbsp;
+                {detailData.ticket.sportsName}&nbsp;
               </div>
-              <div className="text-xl font-extrabold pt-1">
-                &gt;&nbsp;
-              </div>
+              <div className="text-xl font-extrabold pt-1">&gt;&nbsp;</div>
               <div className="text-2xl font-extrabold">
-                {filteredData.stadiumName}&nbsp;[
-                {filteredData.homeTeamName}] vs&nbsp;
-                {filteredData.awayTeamName}
+                {detailData.ticket.stadiumName}&nbsp;[
+                {detailData.ticket.homeTeamName}] vs&nbsp;
+                {detailData.ticket.awayTeamName}
               </div>
             </div>
-            <h2 className="card-title text-3xl">
-              {filteredData.seatInfo}
-            </h2>
+            <h2 className="card-title text-3xl">{detailData.seatInfo}</h2>
             <p className="text-left text-base font-extrabold">
-              사용날짜:{" "}
-              {formatDate(filteredData.expirationAt)}
+              사용날짜: {formatDate(detailData.ticket.expirationAt)}
             </p>
             <p className="text-left text-base font-extrabold">
-              판매자: {filteredData.memberName}
+              판매자 id: {detailData.memberId}
             </p>
             <p className="text-sm text-end justify-end">
-              정가: {filteredData.originalPrice}
+              정가: {detailData.ticket.originalPrice}
             </p>
             <p className="font-extrabold text-xl text-end">
-              수량: {filteredData.quantity}장
-              &nbsp;&nbsp;판매가:&nbsp;
-              {filteredData.salePrice}
+              수량: {detailData.ticket.quantity}장 &nbsp;&nbsp;판매가:&nbsp;
+              {detailData.ticket.salePrice}
             </p>
             <div className="card-actions justify-end">
               <button
@@ -89,7 +90,7 @@ export default function DetailPage() {
                 // onClick={() => handleAddToCart(data)}
               >
                 <svg
-                  id={`likeButton-${filteredData.salePostId}`}
+                  id={`likeButton-${detailData.salePostId}`}
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6"
                   fill="none"
@@ -104,52 +105,62 @@ export default function DetailPage() {
                   />
                 </svg>
               </button>
-              <button className="btn btn-primary">
-                티켓 구매
-              </button>
+              <button className="btn btn-primary">티켓 구매</button>
             </div>
           </div>
         </div>
       </div>
-      <div className="ml-auto flex items-center justify-end w-5/6 mr-4">
-        <div className="mr-4">
-          <input
-            type="checkbox"
-            id="over_price"
-            className="form-checkbox rounded-full bg-yellow-300"
-          />
-          <label
-            htmlFor="over_price"
-            className="font-extrabold text-navy-basic"
-          >
-            정가 이상 등록
-          </label>
-        </div>
-        <div className="mr-4">
-          <input
-            type="checkbox"
-            id="fake_item"
-            className="form-checkbox rounded-full bg-yellow-300"
-          />
-          <label
-            htmlFor="fake_item"
-            className="font-extrabold text-navy-basic"
-          >
-            허위 매물 의심
-          </label>
-        </div>
-        <div className="mr-4">
-          <input
-            type="checkbox"
-            id="inappropriate_pic"
-            className="form-checkbox rounded-full bg-yellow-300"
-          />
-          <label
-            htmlFor="inappropriate_pic"
-            className="font-extrabold text-navy-basic"
-          >
-            부적절한 사진
-          </label>
+      <div className="flex">
+        <div className="ml-auto flex items-center justify-end w-5/6 mr-4">
+          <div className="mr-4">
+            <input
+              type="checkbox"
+              id="over_price"
+              className="form-checkbox rounded-full bg-yellow-300"
+            />
+            <label
+              htmlFor="over_price"
+              className="font-extrabold text-navy-basic"
+            >
+              정가 이상 등록
+            </label>
+          </div>
+          <div className="mr-4">
+            <input
+              type="checkbox"
+              id="fake_item"
+              className="form-checkbox rounded-full bg-yellow-300"
+            />
+            <label
+              htmlFor="fake_item"
+              className="font-extrabold text-navy-basic"
+            >
+              허위 매물 의심
+            </label>
+          </div>
+          <div className="mr-4">
+            <input
+              type="checkbox"
+              id="inappropriate_pic"
+              className="form-checkbox rounded-full bg-yellow-300"
+            />
+            <label
+              htmlFor="inappropriate_pic"
+              className="font-extrabold text-navy-basic"
+            >
+              부적절한 사진
+            </label>
+          </div>
+          <div className="mr-4">
+            <input
+              type="checkbox"
+              id="etc"
+              className="form-checkbox rounded-full bg-yellow-300"
+            />
+            <label htmlFor="etc" className="font-extrabold text-navy-basic">
+              기타
+            </label>
+          </div>
         </div>
         <button className="p-0">
           <img src={ReportBtn} className="w-24" />
