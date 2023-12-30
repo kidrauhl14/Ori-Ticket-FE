08432de55979508192f844e926164dd110f9e582
gxvelxx <wolf2005k@naver.com>
2023-12-31 02:08:25 +0900

    :white_check_mark: ProfilePage API 연동
    ProfilePage에서 판매중, 판매종료 API연동 완료하였습니다.

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 // 이미지
 import Ticket from "@assets/img_ticket.png";
@@ -13,31 +14,6 @@ import {
   shoppingCartState,
   shoppingCartTotalState,
 } from "../store/shoppingCart";
-
-// const saleTicket = [
-//   {
-//     sport_name: "야구",
-//     team_name: "삼성",
-//     stadium_name: "대구 라이온즈 파크",
-//     seat_info: "7구역 b열 3층 1루 지정석",
-//     use_date: "사용일: 11-25-2023 (Saturday)",
-//     quantity: "1개",
-//     original_price: "20,000",
-//     sale_price: "20,000",
-//     post_date: "11-25-2023",
-//   },
-//   {
-//     sport_name: "야구",
-//     team_name: "롯데",
-//     stadium_name: "부산 사직 야구장",
-//     seat_info: "35구역 e열 2층 2루 지정석",
-//     use_date: "사용일: 11-23-2023 (Thursday)",
-//     quantity: "1개",
-//     original_price: "20,000",
-//     sale_price: "17,000",
-//     post_date: "11-25-2023",
-//   },
-// ];
 
 export default function ProfilePage() {
   // 날짜를 년-월-일 형식으로 변환하는 함수
@@ -59,11 +35,140 @@ export default function ProfilePage() {
   );
   const shoppingCart = useRecoilValue(shoppingCartState);
 
-  // 탭 메뉴
-  const [selectedTab, setSelectedTab] = useState("sell");
+  // 탭 기눙
+  const [selectedTab, setSelectedTab] = useState("selling");
 
-  const handleTabChange = (tab) => {
+  const handleTabChange = async (tab) => {
     setSelectedTab(tab);
+
+    try {
+      let response;
+
+      if (tab === "selling") {
+        response = await axios.get(
+          `https://oriticket.link/members/${memberId}/posts?status=for_sale,trading`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setSellingTickets(response.data);
+      } else if (tab === "soldout") {
+        response = await axios.get(
+          `https://oriticket.link/members/${memberId}/posts?status=sold,reported`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setSoldoutTickets(response.data);
+      } else if (tab === "trading") {
+        // 거래 중인 티켓 정보 가져오기
+        // ...
+      } else if (tab === "traded") {
+        // 거래 종료된 티켓 정보 가져오기
+        // ...
+      }
+
+      if (response.status !== 200) {
+        throw new Error(
+          `서버 응답 오류: ${response.status}`
+        );
+      }
+    } catch (error) {
+      console.error(
+        `${tab} 탭 정보를 불러오는 중 에러:`,
+        error.message
+      );
+    }
+  };
+
+  // 판매 중인 티켓 정보를 담을 상태
+  const [sellingTickets, setSellingTickets] = useState([]);
+  // 판매 종료된 티켓 정보를 담을 상태
+  const [soldoutTickets, setSoldoutTickets] = useState([]);
+
+  // 사용자의 ID
+  const memberId = "1"; // memberId를 사용자의 실제 ID로 대체
+
+  useEffect(() => {
+    // 판매 중인 티켓 정보를 가져오는 함수
+    const fetchSellTickets = async () => {
+      try {
+        const response = await axios.get(
+          `https://oriticket.link/members/${memberId}/posts?status=for_sale,trading`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.status !== 200) {
+          throw new Error(
+            `서버 응답 오류: ${response.status}`
+          );
+        }
+
+        setSellingTickets(response.data);
+      } catch (error) {
+        console.error(
+          "판매 중인 티켓 정보를 불러오는 중 에러:",
+          error.message
+        );
+      }
+    };
+
+    fetchSellTickets();
+  }, [memberId]);
+
+  const showSellTicketList = (tickets) => {
+    return (
+      <div className="mt-4">
+        {tickets.length > 0 ? (
+          tickets.map((ticket, index) => (
+            <div
+              className="card-compact w-full my-4 bg-base-100 shadow-xl"
+              key={index}
+            >
+              <div className="card-body">
+                <div className="flex">
+                  <div className="text-xl font-extrabold pt-1">
+                    {ticket.ticket.sportsName}
+                  </div>
+                  <div className="text-xl font-extrabold pt-1"></div>
+                  <div className="text-2xl font-extrabold">
+                    {ticket.ticket.stadiumName} [
+                    {ticket.ticket.homeTeamName}] vs
+                    {ticket.ticket.awayTeamName}
+                  </div>
+                </div>
+                <h2 className="card-title text-3xl">
+                  {ticket.ticket.seatInfo}
+                </h2>
+                <p className="text-left text-base font-extrabold">
+                  사용날짜:{" "}
+                  {formatDate(ticket.ticket.expirationAt)}
+                </p>
+                <p className="text-sm text-end justify-end">
+                  정가: {ticket.ticket.originalPrice}
+                </p>
+                <p className="font-extrabold text-xl text-end">
+                  수량: {ticket.ticket.quantity}장 판매가:
+                  {ticket.ticket.salePrice}
+                </p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="text-2xl text-center text-gray-500 w-full py-4 bg-base-100 shadow-xl">
+            해당하는 티켓이 없습니다.
+          </div>
+        )}
+      </div>
+    );
   };
 
   return (
@@ -179,9 +284,9 @@ export default function ProfilePage() {
         <div className="flex-col">
           <div className="flex justify-between">
             <div
-              onClick={() => handleTabChange("sell")}
+              onClick={() => handleTabChange("selling")}
               className={`flex justify-center ml-1 w-32 font-bold text-lg border-4 shadow-xl ${
-                selectedTab === "sell"
+                selectedTab === "selling"
                   ? "border-blue-950 bg-blue-950 text-white"
                   : "border-gray-50 bg-white text-blue-950"
               } rounded-xl cursor-pointer`}
@@ -189,9 +294,9 @@ export default function ProfilePage() {
               판매중
             </div>
             <div
-              onClick={() => handleTabChange("sold")}
+              onClick={() => handleTabChange("soldout")}
               className={`flex justify-center ml-1 w-32 font-bold text-lg border-4 shadow-xl ${
-                selectedTab === "sold"
+                selectedTab === "soldout"
                   ? "border-blue-950 bg-blue-950 text-white"
                   : "border-gray-50 bg-white text-blue-950"
               } rounded-xl cursor-pointer`}
@@ -199,9 +304,9 @@ export default function ProfilePage() {
               판매종료
             </div>
             <div
-              onClick={() => handleTabChange("trade")}
+              onClick={() => handleTabChange("trading")}
               className={`flex justify-center ml-1 w-32 font-bold text-lg border-4 shadow-xl ${
-                selectedTab === "trade"
+                selectedTab === "trading"
                   ? "border-blue-950 bg-blue-950 text-white"
                   : "border-gray-50 bg-white text-blue-950"
               } rounded-xl cursor-pointer`}
@@ -209,9 +314,9 @@ export default function ProfilePage() {
               거래중
             </div>
             <div
-              onClick={() => handleTabChange("complete")}
+              onClick={() => handleTabChange("traded")}
               className={`flex justify-center ml-1 w-32 font-bold text-lg border-4 shadow-xl ${
-                selectedTab === "complete"
+                selectedTab === "traded"
                   ? "border-blue-950 bg-blue-950 text-white"
                   : "border-gray-50 bg-white text-blue-950"
               } rounded-xl cursor-pointer`}
@@ -219,67 +324,15 @@ export default function ProfilePage() {
               거래종료
             </div>
           </div>
+          {/* 판매중인 티켓 리스트 */}
+          {selectedTab === "selling" &&
+            showSellTicketList(sellingTickets)}
+          {/* 판매종료 티켓 리스트 */}
+          {selectedTab === "soldout" &&
+            showSellTicketList(soldoutTickets)}
+          {/* 판매중인 티켓 리스트 끝 */}
         </div>
         {/* 거래 완료 */}
-        {/* <div className="flex-row mb-8">
-          <div className="flex justify-center ml-1 w-32 font-bold text-lg border-4 shadow-xl border-blue-950 bg-blue-950 text-white rounded-xl">
-            거래중
-          </div>
-          <div className="flex justify-center ml-1 w-32 font-bold text-lg border-4 shadow-xl border-blue-950 bg-blue-950 text-white rounded-xl">
-            거래 완료
-          </div>
-          <div>
-            {saleTicket.map((ticket, index) => (
-              <div
-                key={index}
-                className="flex w-full h-full rounded-xl border-2 border-blue-950"
-              >
-                <div className="w-72">
-                  <div className="flex m-2">
-                    <p className="text-xs mr-1">
-                      {ticket.sport_name}
-                    </p>
-                    <p className="text-xs font-extrabold mr-1">
-                      &gt;
-                    </p>
-                    <p className="text-xs mr-1">
-                      {ticket.team_name}
-                    </p>
-                    <p className="text-xs font-extrabold mr-1">
-                      &gt;
-                    </p>
-                    <p className="text-xs">
-                      {ticket.stadium_name}
-                    </p>
-                  </div>
-                  <div className="flex-col m-2">
-                    <div className="text-xl text-left font-extrabold">
-                      {ticket.seat_info}
-                    </div>
-                    <div className="text-left font-semibold">
-                      {ticket.use_date}
-                    </div>
-                  </div>
-                </div>
-                <div className="w-28 grid justify-center items-center text-sm">
-                  {ticket.quantity}
-                </div>
-                <div className="w-28 flex-col">
-                  <div className="text-sm text-end mt-6 pr-6 justify-end">
-                    {ticket.original_price}
-                  </div>
-                  <div className="font-extrabold text-xl text">
-                    {ticket.sale_price}
-                  </div>
-                </div>
-                <div className="w-28 grid justify-center items-center text-sm">
-                  {ticket.post_date}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div> */}
-        {/* 거래 완료끝 */}
       </div>
     </div>
   );
