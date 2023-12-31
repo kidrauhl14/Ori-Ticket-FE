48eae4a48e01b6e74b686f375ee9228b9704ef75
Eunchae <ddungal3@gmail.com>
2024-01-01 03:32:26 +0900

    :sparkles: Feat: SignupPage(회원정보 등록페이지)에서 휴대폰 번호 인증 API연동 성공, 회원정보 등록기능 추가 구현

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -3,17 +3,27 @@ import {useNavigate} from "react-router-dom";
 import { useState } from "react";
 import Datepicker from "react-tailwindcss-datepicker";
 import { useRecoilValue, useSetRecoilState } from "recoil";
+import { loginState } from "@recoil/loginState";
+import { userInfoState } from "@recoil/userInfoState";
 
 export default function SignupPage(){
   const { replace } = useNavigate();
-  const [isChecked, setIsChecked] = useState([false,false,false,false]);
+  const [isChecked, setIsChecked] = useState([false, false, false, false]);
 
   // "등록할" 회원정보를 관리할 상태 객체
   const [signupInfo, setSignupInfo] = useState({
+    email:"",
     name: "",
     birthDate: "",
     phoneNum: "",
   });
+
+  console.log(signupInfo.email);
+
+  const [verification, setVerification] = useState("");
+  
+  // 가입하기 버튼 상태 (활성화/비활성화)
+  const [isSubmitButtonEnabled, setIsSubmitButtonEnabled] = useState(false);
 
   const toggleCheckbox = (index) => {
     const newChecked = [...isChecked];
@@ -21,18 +31,40 @@ export default function SignupPage(){
     setIsChecked(newChecked);
   };
 
-  const register = (e) => {
-    e.preventDefault()
+  const sendVerification = () => {
     axios
-      .post("/members/signup", signupInfo)
+      .post("http://13.124.46.138:8080/send-one", {
+        phoneNum: signupInfo.phoneNum,
+      })
       .then((res) => {
-        // userInfo에 id값이 들어온다.
+        // 들어온 인증번호값과 사용자가 입력한 인증번호값이 같은지 확인
+        // 같으면, 가입하기 버튼을 누를 수 있음
+        console.log("인증번호다!", res.data.getSubject);
+        console.log("인증번호다!", res.data);
+        setVerification(res.data.getSubject);
+        console.log(signupInfo);
+      })
+      .catch((error) => {
+        console.log("인증번호 전송 중 오류 발생:", error);
+      });
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://13.124.46.138:8080/members/signup", signupInfo)
+      .then((res) => {
         replace("/");
+        console.log("로그인 성공!");
+        console.log(signupInfo);
       })
       .catch((error) => {
         console.log("An error occurred:", error);
+        console.log(signupInfo);
       });
-  }
+  };
+
+
 
   return (
     <>
@@ -40,6 +72,23 @@ export default function SignupPage(){
         <h1 className=" font-extrabold">회원가입</h1>
         <div className="grid-rows-6 w-full my-10">
           <form onSubmit={register} action="/members/signup" method="post">
+            <div className="py-4 text-left">
+              <label htmlFor="email" className="font-bold">
+                이메일
+              </label>
+              <br />
+              <input
+                type="email"
+                id="email"
+                className="w-full appearance-none border border-4 rounded-lg border-navy-basic text-gray-700 placeholder-gray-400 focus:border-transparent"
+                placeholder="ori_ticket@gmail.com"
+                value={signupInfo.email}
+                onChange={(e) => {
+                  setSignupInfo({ ...signupInfo, email: e.target.value });
+                }}
+                required
+              />
+            </div>
             <div className="py-4 text-left">
               <label htmlFor="username" className="font-bold">
                 이름
@@ -69,8 +118,10 @@ export default function SignupPage(){
                 dateFormat="MM-dd-yyyy"
                 id="birthdate"
                 name="birthDate"
-                onChange={(newDate) => {
-                  setSignupInfo({ ...signupInfo, birthDate: newDate });
+                onChange={({ startDate }) => {
+                  const formattedDate = `${startDate}T00:00:00`;
+                  setSignupInfo({ ...signupInfo, birthDate: formattedDate });
+                  console.log(formattedDate);
                 }}
                 className="w-full"
                 required
@@ -79,15 +130,45 @@ export default function SignupPage(){
             <div className="py-4 text-left">
               <label htmlFor="phone">휴대폰 번호</label>
               <br />
+              <div className="flex">
+                <input
+                  type="tel"
+                  name="phoneNum"
+                  id="phone"
+                  placeholder="01012345678"
+                  className="w-full appearance-none border border-4 rounded-lg border-navy-basic text-gray-700 placeholder-gray-400 focus:border-transparent"
+                  value={signupInfo.phoneNum}
+                  onChange={(e) => {
+                    setSignupInfo({ ...signupInfo, phoneNum: e.target.value });
+                  }}
+                  required
+                />
+                <button
+                  type="button"
+                  className="w-16 p-0 ml-2 bg-navy-basic text-white rounded-lg"
+                  onClick={(e) => {
+                    sendVerification();
+                  }}
+                >
+                  인증
+                </button>
+              </div>
+            </div>
+            <div className="py-4 text-left">
+              <label htmlFor="auth" className="font-bold">
+                인증번호
+              </label>
+              <br />
               <input
-                type="tel"
-                name="phoneNum"
-                id="phone"
-                placeholder="하이픈(-) 제외한 숫자만 입력"
+                type="text"
+                id="auth"
                 className="w-full appearance-none border border-4 rounded-lg border-navy-basic text-gray-700 placeholder-gray-400 focus:border-transparent"
-                value={signupInfo.phoneNum}
+                placeholder="123456"
                 onChange={(e) => {
-                  setSignupInfo({ ...signupInfo, phoneNum: e.target.value });
+                  // e.target.value랑 verification값이 같은지 확인
+                  // 만약에 둘이 같으면, 가입하기 버튼을 활성화하고, 둘이 다르면 가입하기 버튼을 비활성화
+                  const isVerificationMatched = e.target.value === verification;
+                  setIsSubmitButtonEnabled(isVerificationMatched);
                 }}
                 required
               />
@@ -106,7 +187,7 @@ export default function SignupPage(){
                   약관 전체동의
                 </label>
               </div>
-              <div className="bg-pink-100 flex items-center mb-2">
+              <div className="flex items-center mb-2">
                 <input
                   checked={isChecked[1]}
                   type="checkbox"
@@ -149,7 +230,10 @@ export default function SignupPage(){
             <div>
               <button
                 type="submit"
-                className="py-4 mt-8 bg-navy-basic text-white"
+                className={`py-4 mt-8 bg-navy-basic text-white ${
+                  isSubmitButtonEnabled ? "" : "opacity-50 cursor-not-allowed"
+                }`}
+                disabled={!isSubmitButtonEnabled}
               >
                 가입하기
               </button>
