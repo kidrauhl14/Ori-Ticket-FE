ed27a9fd397c7b2f859faa2bd276440d24501202
Eunchae <ddungal3@gmail.com>
2023-12-31 11:14:00 +0900

    KakaoRedirect axios 요청수정

--- src/pages/login/KakaoRedirect.jsx
+++ src/pages/login/KakaoRedirect.jsx
@@ -12,59 +12,36 @@ export default function KakaoRedirect() {
   const navigate = useNavigate();
   const code = new URL(document.location.toString()).searchParams.get("code");
   // const {fetchDataUseAxios}= useAxios();
-
+  console.log("KakaoRedirect컴포넌트가 실행이 된걸까?");
+  
   useEffect(() => {
-    // 인가코드 백엔드로 보내기
-    axios
-      .get("http://13.124.46.138:8080/members/kakao/login", { code })
-      .then((response) => {
-        if (response && response.status === 200) {
-          console.log("인가코드 제대로 보내졌고, response 받음", response);
-          // 인가코드 보내고나서 response값으로 받은 개인정보에서, email만 백엔드로 보냄
-          axios
-            .post("http://13.124.46.138:8080/members/signin", {
-              email: response.data.email,
-            })
-            .then((response) => {
-              // email보내고나서, 응답이 true면 메인페이지로, false면 회원가입 페이지로 이동
-              if (response.data) {
-                navigate("/");
-              } else {
-                navigate("/signup");
-              }
-            });
-        }
-      })
-      .catch((error) => {
-        console.error("카카오 로그인 에러:", error);
-      });
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response1 = await axios.get(
+          "http://13.124.46.138:8080/members/kakao/login",
+          { code }
+        );
+        console.log("인가코드 제대로 보내졌고, response 받음", response1);
 
-  // useEffect(() => {
-  //   fetchDataUseAxios("defaultAxios", {
-  //     method: "post",
-  //     url: "https://oriticket.link/members/kakao/login",
-  //     data: { code },
-  //   })
-  //     .then((response) => {
-  //       if (response && response.status === 200) {
-  //         fetchDataUseAxios("defaultAxios", {
-  //           method: "post",
-  //           url: "https://oriticket.link/members/signin",
-  //           data: { email: response.data.email },
-  //         }).then((response) => {
-  //           if (response.data) {
-  //             navigate("/");
-  //           } else {
-  //             navigate("/signup");
-  //           }
-  //         });
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error("카카오 로그인 에러:", error);
-  //     });
-  // }, []);
+        const response2 = await axios.post(
+          "http://13.124.46.138:8080/members/signin",
+          {
+            email: response1.data.email,
+          }
+        );
+
+        if (response2.data) {
+          navigate("/");
+        } else {
+          navigate("/signup");
+        }
+      } catch (error) {
+        console.error("카카오 로그인 에러:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
